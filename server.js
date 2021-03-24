2129045fc72fb310c826b607b60fc73b94d795b1
Joseph Anderson <josephanderson@upperaltitude.com>
2021-03-24 10:14:08 -0400

    Continuing to evolve the server.

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-// Thanks to: https://medium.com/@amitgupta15/node-js-server-without-express-bf22903355ad
-
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
 const url = require('url');
 
 const server = {};
+
+// The base directory to serve files from.
 const baseDir = path.join(__dirname, './');
 
+// The file types we serve.
 const mimeTypes = {
+    '.txt': 'text/plain',
     '.html': 'text/html',
     '.jpg': 'image/jpeg',
     '.jpeg': 'image/jpeg',
@@ -20,11 +22,13 @@ const mimeTypes = {
 };
 
 server.getContentType = url => {
-  // Set the default content type to application/octet-stream
-    let contentType = 'application/octet-stream';
+    // Set the default content type.
+    let contentType = 'application/json';
 
+    // Get the extension of the URL.
     const extname = path.extname(url);
 
+    // Map through to match up the extension to a mime type.
     for (let key in mimeTypes) {
         if (mimeTypes.hasOwnProperty(key)) {
             if (extname === key) {
@@ -37,20 +41,25 @@ server.getContentType = url => {
 };
 
 server.serveStaticContent = (pathname, response) => {
-  // Get content type based on the file extension
-  const contentType = server.getContentType(pathname);
+    // Set the content type based on the file extension.
+    const contentType = server.getContentType(pathname);
+    response.setHeader('Content-Type', contentType);
 
-  response.setHeader('Content-Type', contentType);
+    // Read the file and send the response to the browser.
+    fs.readFile(`${baseDir}${pathname}`, (error, data) => {
+        if (!error) {
+            response.writeHead(200);
+            response.end(data);
+        } else {
+            let data = {
+                httpCode: 404,
+                message: 'Can\'t find that.',
+            };
 
-  fs.readFile(`${baseDir}${pathname}`, (error, data) => {
-    if (!error) {
-        response.writeHead(200);
-        response.end(data);
-    } else {
-        response.writeHead(404);
-        response.end('404 - File Not Found');
-    }
-  });
+            response.writeHead(404);
+            response.end(JSON.stringify(data));
+        }
+    });
 };
 
 // Create the server.
@@ -61,7 +70,7 @@ const httpServer = http.createServer((request, response) => {
 
 server.init = (port, host) => {
     httpServer.listen(port, host, () => {
-        console.log(`Server is listening at http://${host}:${port}`);
+        console.log(`The server is online and listening at http://${host}:${port}!`);
     });
 };
 
